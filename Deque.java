5909b0cd6341108555a985348d8526ea1f81ecc3
KaitlinWan <kwan1@stuy.edu>
2018-04-18 13:58:01 -0400

    Created Interface

--- Deque.java
+++ Deque.java
@@ -1,5 +1,25 @@
 public interface Deque{
+
+    public boolean isEmpty();
+    
+    public int size();
 	
+    public String getFirst();
+
+    public String getLast();
+
+    public void addFirst();
+
+    public void addLast();
+
+    public String peekFirst();
+
+    public String peekLast();
+
+    public String pollFirst();
+
+    public String pollLast();
+    
 	
 
 
