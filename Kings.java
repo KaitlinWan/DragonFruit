6159e20d8d7ba58a600b3631cadc32a8eb21d4d4
Rohan Ahammed <rahammed@stuy.edu>
2018-04-18 23:49:50 -0400

    updated class Kings

--- Kings.java
+++ Kings.java
@@ -1,6 +1,31 @@
 public class Kings implements Deque{
     //instance vars
-    
+    LLNode<String> _front, _end;
+    int _size;
 
+    //constructor
+    public Kings(){
+	_front = _end = null;
+	_size = 0;
+    }
+
+    //methods specified in Deque API
+    public int size(){
+	return _size;
+    }
+    public boolean isEmpty(){
+	return _size == 0;
+	//can also be done _front == null && _end == null;
+    }
+
+    public String peekFirst(){
+	return _front.getValue();
+    }
+    public String peekLast(){
+	return _end.getValue();
+    }
+
+    public static void main(String[] args){
+    }//end of main method
   
 }//end of class Kings
