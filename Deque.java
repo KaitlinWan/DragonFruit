03b1185adf57c7d07e4da2c36b966ea36f1c79a5
Rohan Ahammed <rahammed@stuy.edu>
2018-04-25 18:59:05 -0400

    added comments to Deque.java

--- Deque.java
+++ Deque.java
@@ -1,22 +1,33 @@
 public interface Deque<T>{
+    //interface Deque based on API
 
+    //Checks if this Deque is empty or not.
+    //Returns true if nothing is in Deque, false if something was added
     public boolean isEmpty();
 
+    //returns the total number of elements added to this Deque
     public int size();
 
+    //Returns the first element of the Deque. Returns null if empty.
+    //Does not remove the first element
     public T peekFirst();
 
+    //Returns the last element of the Deque. Returns null if empty.
+    //Does not remove the last element
     public T peekLast();
 
+    //Inserts the specified element at the front of this deque
     public void addFirst(T value);
 
+    //Inserts the specified element at the end of this deque
     public void addLast(T value);
 
+    //Retrieves and removes the first element of this deque.
+    //Throws an exception if empty
     public T removeFirst();
 
+    //Retrieves and removes the last element of this deque.
+    //Throws an exception if empty
     public T removeLast();
-
-
-
-
-}//end of Deque
+    
+}//end of interface Deque
